cf6e61930ab26f27c386752f17df4cc2308476fd
garycheung13 <garyc102@gmail.com>
2018-06-12 23:40:33 -0400

    add js bundler task and smoke test

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,15 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
 var rename = require('gulp-rename');
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
+var uglify = require('gulp-uglify');
+var sourcemaps = require('gulp-sourcemaps');
 var bs = require("browser-sync").create();
+var babelify = require('babelify');
 
-gulp.task('serve', ['scss'], function() {
+gulp.task('serve', ['scss'], function () {
 
     bs.init({
         server: "."
@@ -26,6 +32,21 @@ gulp.task("scss", function () {
         }))
         .pipe(rename("main.min.css"))
         .pipe(gulp.dest("css/"));
+});
+
+
+gulp.task("js", function () {
+    browserify({
+            entries: "js/main.js",
+            debug: true
+        }).transform("babelify", { presets: ["@babel/preset-es2015"]})
+        .bundle()
+        .pipe(source("main.min.js"))
+        .pipe(buffer())
+        .pipe(sourcemaps.init({loadMaps: true}))
+            .pipe(uglify())
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest("js/"))
 });
 
 gulp.task("default", ["serve"]);
